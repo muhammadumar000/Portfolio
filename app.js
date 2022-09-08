6ebca9f7df544ea42dc283f3030b6dedbeb71c43
muhammadumar000 <FA20-BEE-150@ISBSTUDENT.COMSATS.EDU.PK>
2022-09-08 12:13:14 +0500

    Added preLoader

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const preLoader = document.getElementById('preLoader');
 
-window.addEventListener('load', function(){
+window.addEventListener('DOMContentLoaded', function(){
     preLoader.style.display = 'none';
 })
 
